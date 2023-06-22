6579b3090d1bb9bfefb331e1568a87efb77384eb
Roee Milo <roee.milo@post.runi.ac.il>
2023-06-22 19:53:49 +0300

    last bug fix

--- workerApp.py
+++ workerApp.py
@@ -40,6 +40,14 @@ class Worker:
         else:
             self.SecondManager = ip
 
+    def processWorkById(self, work, manager_ip, workID):
+        buffer = base64.b64decode(work.json()['buffer'])
+        iterations = int(work.json()['iterations'])
+        hash = self.do_work(buffer,iterations)
+        url2 = f"http://{manager_ip}:5000/workIsCompleted?workID={workID}&bufferHash={hash}"
+
+        requests.get(url2)
+        lastTime = datetime.now()
     ##runs until a maximum time limit is reached.
     ##checks for work from two IP addresses and performs the work if available, notifying the completion
     def loop(self):
@@ -54,17 +62,11 @@ class Worker:
                 if manager_ip:
                     url1 = f"http://{manager_ip}:5000/getWork"
                     work = requests.get(url1)
-                    if work.status_code == scode:
-                        workID = work.json()['workID']
+                    workID = work.json()['workID']
+                    if workID:
+                        self.processWorkById(work, manager_ip, workID)
                         if workID:
-                            buffer = base64.b64decode(work.json()['buffer'])
-                            iterations = int(work.json()['iterations'])
-                            hash = self.do_work(buffer,iterations)
-                            url2 = f"http://{manager_ip}:5000/workIsCompleted?workID={workID}&bufferHash={hash}"
-                            requests.get(url2)
-                            lastTime = datetime.now()
-                            if workID:
-                                continue
+                            continue
             time.sleep(timeToSleep)
             d = (datetime.now() - lastTime).total_seconds()
         self.workerDone()
@@ -97,23 +99,25 @@ def run_process():
 
     while True:
         if worker.FirstManager:
-            url1 = f"http://{worker.FirstManager}:5000/getOtherIP"
-            response = requests.get(url1)
-            if response.status_code == scode:
-                otherIP = response.json()['otherIP']
-                worker.update_IP(otherIP, False)
-            else:
-                worker.update_IP(None, True)
+            runWorker(scode, True)
 
         if worker.SecondManager:
-            url2 = f"http://{worker.SecondManager}:5000/getOtherIP"
-            response = requests.get(url2)
-            if response.status_code != scode:
-                worker.update_IP(None, False)
-            else:
-                otherIP = response.json()['otherIP']
-                worker.update_IP(otherIP, True)
+            runWorker(scode, False)
+
         time.sleep(time_to_sleep)
+
+
+def runWorker(scode,isFirst):
+    if isFirst:
+        url = f"http://{worker.FirstManager}:5000/getSecondaryIP"
+    else:
+        url = f"http://{worker.SecondManager}:5000/getSecondaryIP"
+    response = requests.get(url)
+    if response.status_code == scode:
+        secondaryIP = response.json()['secondaryIP']
+        worker.update_IP(secondaryIP, not isFirst)
+    else:
+        worker.update_IP(None, isFirst)
 
 
 timer_thread = threading.Thread(target=run_process)
